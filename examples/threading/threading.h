f9c8b9f83fb6e5e78f85101fee42f39928a13371
spinoandraptos <spinoandraptos@gmail.com>
2024-09-20 07:07:22 +0800

    Tested threading with local unit test script, passed 4 tests

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -8,18 +8,21 @@
  * the joiner thread.
  */
 struct thread_data{
-    /*
-     * TODO: add other values your thread will need to manage
-     * into this structure, use this structure to communicate
-     * between the start_thread_obtaining_mutex function and
-     * your thread implementation.
-     */
 
-    /**
-     * Set to true if the thread completed with success, false
-     * if an error occurred.
-     */
-    bool thread_complete_success;
+	/* Pointer to mutex */
+	pthread_mutex_t* mutex;
+
+	/* Time to wait before claiming mutex */
+	int wait_mutex_time;
+
+	/* Time to wait before releasing mutex */
+	int lock_mutex_time;
+
+	/**
+	* Set to true if the thread completed with success, false
+	* if an error occurred.
+	*/
+	bool thread_complete_success;
 };
 
 
